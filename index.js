605c8bc1055b687e6b0574e0657cda95fbcc30bb
gleidsonteixeira <ronier.lim@gmail.com>
2024-10-21 12:08:52 -0300

    updates

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
+const cupomRoutes = require('./src/routes/cupomRoutes')
+
 app.get("/", (req, res) => {
     res.send('e ai');
 })
 
-app.get("/boas-vindas", (req, res) => {
-    res.send('Seja bem-vindo');
-})
+app.use("/cupons", cupomRoutes)
+
 
 app.listen(8000, () => {
     console.log(`Servidor de pé: http://localhost:8000`);
