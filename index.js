3768434ce18cc2167cec2f87db1d26377665659c
unknown <joaosilva0074@digitalcollege.internal>
2024-12-02 11:38:29 -0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
+const bodyParser = require('body-parser');
 
 const usuarioRoutes = require('./src/routes/usuarioRoutes');
 const cupomRoutes = require('./src/routes/cupomRoutes');
@@ -7,12 +10,14 @@ const produtoRoutes = require('./src/routes/produtoRoutes');
 
 const { verificarToken } = require("./src/utils");
 
+app.use(bodyParser.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/files', express.static('./src/uploads/produtos_imagem'));
 
 app.get("/", (req, res) => {
-    res.send('hello world');
+    res.redirect('/docs');
 })
 
 app.use("/usuarios", usuarioRoutes);
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -4,6 +4,7 @@ const { verificarToken } = require('../utils');
 const router = require('express').Router();
 
 router.get('/', verificarToken, async (req, res) => {
+    // #swagger.description = 'Retorna lista de usuarios'
     res.send(await listarTodosUsuarios());
 })
 
@@ -12,6 +13,15 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+    // #swagger.description = 'Cria um usuario'
+    /* #swagger.parameters['obj'] = {
+    in: 'body',
+    schema: {
+        $usuario_email: "email@email.com",
+        $usuario_senha: "min-8 dig+1-letra-maiscula+1-alfanumerico",
+        $usuario_nome: "Nome do Usuario",
+        }
+    } */ 
     res.send(await criarUsuario(req.body))
 })
 
